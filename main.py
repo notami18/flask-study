002a45b6005076f27fef169c856f759e37037d0e
Andres Minota <andresnotami@gmail.com>
2024-02-27 08:51:37 -0500

    Agrega archivos y realiza cambios en el código de la aplicación Flask

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from flask import Flask, request, redirect, make_response, render_template
+from flask import Flask, request, redirect, make_response, render_template, session
+from flask_bootstrap import Bootstrap
 
 app: Flask = Flask(__name__)
+bootstrap = Bootstrap(app)
 
-todos: list[str] = ["TODO 1", "TODO 2", "TODO 3"]
+app.config['SECRET_KEY'] = 'Super-secret-key'
 
+todos: list[str] = ["Comprar cafe", "Enviar solicitud de compra", "Producir un mensaje"]
+
+@app.errorhandler(404)
+def page_not_found(error: Exception):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def internal_server_error():
+    return render_template('500.html')
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'todos': todos
